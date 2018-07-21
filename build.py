71d5bd7e0a7c3a5eb99be6710c711aaf528a1fa8
Ilya Andreev <mrbrikster@yandex.ru>
2018-07-21 21:57:47 +0300

    Some fix #2

--- build.py
+++ build.py
@@ -38,7 +38,7 @@ for filename in os.listdir(dir_src):
     shutil.copyfile(dir_src + filename, dir_dst + filename)
 
 for i in range(0, 10):
-	print('Generating page ' + i + '...')
+	print('Generating page ' + str(i) + '...')
 	for k in range(0, 256):	
 		set_cell('temp/assets/minecraft/textures/font/unicode_page_9{page}.png'.format(page=i),
 		k + 1, 'files/images/{page}/{icon}.png'
